49521662d30678326ed8df9d2cc045ca47246917
Plums Maryjo <pmaryjo@ox-r5.msk.21-school.ru>
2021-11-21 17:01:43 +0300

    main got SHLVL updater

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,12 +6,28 @@
 /*   By: pmaryjo <pmaryjo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 15:54:30 by pmaryjo           #+#    #+#             */
-/*   Updated: 2021/11/21 16:42:11 by pmaryjo          ###   ########.fr       */
+/*   Updated: 2021/11/21 17:00:30 by pmaryjo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../include/rebuilt_funcs.h"
+
+void	update_shlvl(void)
+{
+	int			shlvl;
+	t_env_elem	*env_elem;
+
+	env_elem = env_get_by_name(g_data.envp, "SHLVL");
+	if (!env_elem)
+		env_push_back(g_data.envp, "SHLVL=1");
+	else
+	{
+		shlvl = ft_atoi(env_elem->value);
+		free(env_elem->value);
+		env_elem->value = ft_itoa(shlvl + 1);
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -23,6 +39,7 @@ int	main(int argc, char **argv, char **envp)
 		printf("Can not create local enviroment\n");
 		return (1);
 	}
+	update_shlvl();
 	g_data.vars = NULL;
 	g_data.exit_status = 0;
 }
