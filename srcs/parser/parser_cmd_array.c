0098964bf3a5f32ba732e38260fc422706bd3795
Plums Maryjo <pmaryjo@ox-s3.msk.21-school.ru>
2021-12-04 14:32:08 +0300

    aaa

--- srcs/parser/parser_cmd_array.c
+++ srcs/parser/parser_cmd_array.c
@@ -6,11 +6,24 @@
 /*   By: pmaryjo <pmaryjo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 17:51:02 by msalena           #+#    #+#             */
-/*   Updated: 2021/12/04 14:22:47 by pmaryjo          ###   ########.fr       */
+/*   Updated: 2021/12/04 14:31:14 by pmaryjo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/parser.h"
+
+static void	free_if_not_malloc(char **added_str, char ***arr_str)
+{
+	if (*arr_str && *added_str)
+	{
+		free(*added_str);
+		utils_destroy_strings_array(*added_str);
+	}
+	else if (*arr_str)
+		utils_destroy_strings_array(*arr_str);
+	else if (*added_str)
+		free(*added_str);
+}
 
 static char	**arr_cmd_add_realloc(char *added_str, char **arr_str)
 {
@@ -25,15 +38,7 @@ static char	**arr_cmd_add_realloc(char *added_str, char **arr_str)
 	new_arr = (char **)malloc(sizeof(char *) * (tmp_i + 2));
 	if (!new_arr)
 	{
-		if (arr_str && added_str)
-		{
-			free(added_str);
-			utils_destroy_strings_array(added_str);
-		}
-		else if (arr_str)
-			utils_destroy_strings_array(arr_str);
-		else if (added_str)
-			free(added_str);
+		free_if_not_malloc(&added_str, &arr_str);
 		return (NULL);
 	}
 	tmp_i = 0;
@@ -75,7 +80,7 @@ static t_par_elem	*cmd_handler(t_par_elem *substr, char ***cmd_substr)
 	return (substr);
 }
 
-/**/
+/* Formations new arr which consists of cmd strs*/
 char	**arr_cmd_formation(t_par_list *elem_list)
 {
 	t_par_elem	*substr;
