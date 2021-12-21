8c65a3a088b8e5054f5ab70fd93c6341034bce23
Plums Maryjo <pmaryjo@ox-t5.msk.21-school.ru>
2021-12-21 16:43:21 +0300

    file opener now changes last exit status

--- srcs/processes/proc_files.c
+++ srcs/processes/proc_files.c
@@ -6,7 +6,7 @@
 /*   By: pmaryjo <pmaryjo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 18:11:25 by pmaryjo           #+#    #+#             */
-/*   Updated: 2021/12/12 13:51:57 by pmaryjo          ###   ########.fr       */
+/*   Updated: 2021/12/21 16:42:44 by pmaryjo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,27 @@
 
 int	proc_open_file_(char *path, t_file_mode file_mode)
 {
+	int	fd;
+
 	if (!path)
-		return (NON_FD);
+		fd = NON_FD;
 	if (file_mode == READ)
-		return (open(path, O_RDONLY));
+		fd = open(path, O_RDONLY);
 	else if (file_mode == WRITE)
-		return (open(path, O_WRONLY | O_CREAT | O_TRUNC, 0700));
+		fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0700);
 	else if (file_mode == WRITE_APP)
-		return (open(path, O_WRONLY | O_CREAT | O_APPEND, 0700));
+		fd = open(path, O_WRONLY | O_CREAT | O_APPEND, 0700);
 	else if (file_mode == READ_WRITE)
-		return (open(path, O_RDWR));
+		fd = open(path, O_RDWR);
 	else if (file_mode == READ_WRITE_APP)
-		return (open(path, O_RDWR | O_TRUNC));
-	return (NON_FD);
+		fd = open(path, O_RDWR | O_TRUNC);
+	else
+		fd = NON_FD;
+	if (fd < 0)
+		g_data.exit_status = 1;
+	else
+		g_data.exit_status = 0;
+	return (fd);
 }
 
 // Open file with specified mode
